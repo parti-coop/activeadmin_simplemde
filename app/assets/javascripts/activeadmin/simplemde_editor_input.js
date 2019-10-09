0208aa243fd9453cc696b248c19ee9cb8664abdb
dalikim <dalikim@parti.xyz>
2019-10-09 17:04:00 +0900

    get an instance of SimpleMDE

--- app/assets/javascripts/activeadmin/simplemde_editor_input.js
+++ app/assets/javascripts/activeadmin/simplemde_editor_input.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.simplemde-editor').each(function () {
         var options = { element: $(this).get(0) };
         options = $.extend({}, options, $(this).data('options'));
-        new SimpleMDE(options);
+        var editor = new SimpleMDE(options);
+        $(this).data({editor: editor});
     });
 });
